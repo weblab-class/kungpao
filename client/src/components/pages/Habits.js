ba56b8d7ea4d25b9d5db4a93f8b0bbd228617efa
Andrea Lin <andrealin2018@gmail.com>
2020-01-15 17:33:03 -0500

    checkboxes

--- client/src/components/pages/Habits.js
+++ client/src/components/pages/Habits.js
@@ -14,10 +14,12 @@ class Habits extends Component {
     render() {
         return ( <>
             <h1>Keep track of your habits.</h1>
-            <h2>Daily</h2>
+            <button type="button">daily</button>
+            <button type="button">weekly</button>
+            <button type="button">monthly</button>
             <ul>
-                <li>drink 3 glasses of water</li>
-                <li>eat breakfast</li>
+                <li><input type="checkbox"/>drink 3 glasses of water</li>
+                <li><input type="checkbox"/>eat breakfast</li>
                 <li><input type="text" name="firstname"/><button type="button">+</button></li>
             </ul>
             
