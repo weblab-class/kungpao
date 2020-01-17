a7674c1352fc5065eaca7053f5ff10cfed6b6f66
Andrea Lin <andrealin2018@gmail.com>
2020-01-17 14:53:13 -0500

    fixing login for deployment

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -11,7 +11,7 @@ import FishPopup from "./modules/FishPopup.js";
 import Login from "./pages/Login.js";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "707474204069-ibaig6vr8u2gf995465eel35t6kf6u1r.apps.googleusercontent.com";
 
 import "./App.css";
 import "../utilities.css";
--- client/src/components/pages/Skeleton.js
+++ client/src/components/pages/Skeleton.js
@@ -5,7 +5,7 @@ import "../../utilities.css";
 import "./Skeleton.css";
 
 //TODO: REPLACE WITH YOUR OWN CLIENT_ID
-const GOOGLE_CLIENT_ID = "121479668229-t5j82jrbi9oejh7c8avada226s75bopn.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "707474204069-ibaig6vr8u2gf995465eel35t6kf6u1r.apps.googleusercontent.com";
 
 class Skeleton extends Component {
   constructor(props) {
