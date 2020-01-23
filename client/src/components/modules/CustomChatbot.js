7cf058db96e401d5fc312a96d39c694b87d1ae46
Cindy Wang <cindywang2001@gmail.com>
2020-01-23 16:29:26 -0500

    chatbot now decreases money when you buy stuff, but need to reload for changes to show

--- client/src/components/modules/CustomChatbot.js
+++ client/src/components/modules/CustomChatbot.js
@@ -2,6 +2,7 @@ import React from "react";
 import ChatBot from "../../third_party/chatbot/ChatBot";
 import { get, post } from "../../utilities";
 import ray from "../data/raypfp.jpg";
+import { PromiseProvider } from "mongoose";
 
 
 
@@ -12,11 +13,16 @@ function CustomChatbot(props) {
       height: "65vh",
       botAvatar: ray,
       handleEnd: (response) => {
-        if(response.values[0]!=='No'){
-          const body = { type: response.values[0].type, price: response.values[0].price };
+        const confirmIndex = response.values.indexOf("confirm");
+        if(confirmIndex!==-1){
+          const purchasedfish = response.values[confirmIndex-1];
+          
+          const body = { type: purchasedfish.type };
+          
           post("/api/buyfish", body).then(res => console.log(res));
-          console.log('good')
+          console.log('yay' + purchasedfish.price)
           props.boughtFish(body);
+          props.changeMoney(purchasedfish.price);
 
         }
         
@@ -30,12 +36,12 @@ function CustomChatbot(props) {
       return {
         value: f,
         label: f.name + "$#$" + "$" + f.price + "$#$"+props.displayFish(f.type),
-        trigger: props.money > f.price ? "Done" : "poor"
+        trigger: props.money > f.price ? "sure?" : "poor"
       }
     })
    fishOfferings.push({ 
         value: "No",
-        label: "No",
+        label: "No thanks",
         trigger: "none",
       } )
   const steps = [
@@ -50,6 +56,12 @@ function CustomChatbot(props) {
        trigger: "fish"
       },
       {
+        id: "reoffer",
+        message: "Would you like anything else?",
+        trigger: "offer",
+
+      },
+      {
         id: "fish",
         options: fishOfferings
       },
@@ -60,19 +72,31 @@ function CustomChatbot(props) {
        },
        {
          id: "poor",
-         message: "You don't have enough money to purchase this fish",
+         message: "You don't have enough money to purchase this fish! Why don't you get a different one?",
+         trigger: "offer",
        },
       {
        id: "Done",
        message: "Have a great day !!",
-       trigger: "confirm"
-      },{
+       end: true,
+      },
+      {
+        id: "sure?",
+        message: "Are you sure?",
+        trigger: "confirm",
+      },
+      {
         id: "confirm",
-        end: true,
         options: [{
-          value: "confirm purchase",
-          trigger: "Greet",
-          label: "confirm purchase"
+          value: "confirm",
+          trigger: "Done",
+          label: "confirm purchase",
+        },
+        {
+          value: "No",
+          trigger: "reoffer",
+          label: "cancel",
+
         }]
       }
     ];
