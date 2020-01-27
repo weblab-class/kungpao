5f25d03acde4e4bee65f9e71fc28533ecfbe2f9f
Cindy Wang <cindywang2001@gmail.com>
2020-01-27 11:35:51 -0800

    Fix chat bot message box component

--- client/src/third_party/chatbot/schemas/textSchema.js
+++ client/src/third_party/chatbot/schemas/textSchema.js
@@ -40,6 +40,12 @@ export default [
     required: false
   },
   {
+    key: 'showInput',
+    types: ['boolean'],
+    required: false
+  },
+
+  {
     key: 'hideExtraControl',
     types: ['boolean'],
     required: false
